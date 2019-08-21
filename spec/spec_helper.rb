124be64b2e2236b541342906ddc442d0112c2734
Lessllie Real <rlessllie@yahoo.com>
2019-08-21 05:38:45 +0000

    Done.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,8 +2,43 @@ require 'yaml'
 
 RSpec.configure do |config|
 
-  def __
-    # raise "replace underscore with code"
+  def my_hash
+    my_hash= {
+      :lastname => "Luca"
+    }
   end
 
 end
+
+def new_hash(Hash)
+  Hash.new
+end
+
+
+def pioneer(insect)
+  insect= {
+    :name => 'Grace Hopper'
+  }
+end
+
+def id_generator(integer)
+  integer= {
+    :id => 6
+  }
+end 
+
+def my_hash_creator( hash, key)
+  name= {:hash => key}
+end 
+
+def read_from_hash(hash, key)
+  
+end 
+
+def update_counting_hash(hash, key)
+  fruit= {:apples => 4 , :pears => 3}
+fruit.each |fruit| do
+  fruit += 1
+end
+end 
+
